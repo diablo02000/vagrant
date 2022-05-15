1ad8ea0eecd77be852e917f87dfdd98534fd0928
diablo02000 <christophe.biguereau@gmail.com>
2022-05-15 20:06:24 +0200

    fix: Remove duplicate host entry in /etc/hosts file and install nmap

--- common/bootstrap.sh
+++ common/bootstrap.sh
@@ -3,8 +3,14 @@
 # Bootstrap Debian base VMs.
 set -euxo pipefail
 
+# Get current hostname
+declare -r VM_HOSTNAME="$(hostname -s)"
+
 # Refresh repository list
 apt-get update
 
 # Install packages.
-apt-get install -y htop curl wget dnsutils vim apt-transport-https net-tools jq
+apt-get install -y htop curl wget dnsutils vim apt-transport-https net-tools jq nmap
+
+# Remove duplicate name entry on /etc/hosts
+sed -i '/127.0.1.1 '${VM_HOSTNAME}'/d' /etc/hosts
