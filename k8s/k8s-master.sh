003bac9a699f8ca63ae63b300eee7edeb9fd6283
diablo02000 <christophe.biguereau@gmail.com>
2022-05-15 20:23:43 +0200

    fix: Clean setup master script.

--- k8s/k8s-master.sh
+++ k8s/k8s-master.sh
@@ -4,10 +4,10 @@
 set -euxo pipefail
 
 # Get IPV4 master ip
-declare -r MASTER_IP="$(ip -j -o -4 addr list eth0 | jq -r '.[].addr_info[].local')"
+declare -r MASTER_IP="$(ip -j -o -4 addr list eth1 | jq -r '.[].addr_info[].local')"
 
 # Get current hostname
-declare -r HOSTNAME="$(hostname -s)"
+declare -r VM_HOSTNAME="$(hostname -s)"
 
 # Define POD CIDR
 declare -r POD_CIDR="172.16.0.0/16"
@@ -18,30 +18,41 @@ declare -r ROOT_SHARED_FOLDER="/vagrant/k8s-config"
 # Define Kubernetes dashboard version
 declare -r K8S_DASHBOARD_VERSION="v2.5.1"
 
-# Init k8s master
-sudo kubeadm init --apiserver-advertise-address="${MASTER_IP}" --apiserver-cert-extra-sans="${MASTER_IP}" --pod-network-cidr="${POD_CIDR}" --node-name "${HOSTNAME}"
+##############
+# Kubernetes #
+##############
+# Init kubernetes cluster.
+sudo kubeadm init --apiserver-advertise-address="${MASTER_IP}" --apiserver-cert-extra-sans="${MASTER_IP}" --pod-network-cidr="${POD_CIDR}" --node-name "${VM_HOSTNAME}"
 
-mkdir -p "$HOME"/.kube
-sudo cp -i /etc/kubernetes/admin.conf "$HOME"/.kube/config
-sudo chown "$(id -u)":"$(id -g)" "$HOME"/.kube/config
+# Setup credential configuration for root and vagrant user
+mkdir -p "${HOME}/.kube"
+sudo cp -i /etc/kubernetes/admin.conf "${HOME}/.kube/config"
+sudo chown "$(id -u)":"$(id -g)" "${HOME}/.kube/config"
 
-# Remove older config shared folder
+sudo -i -u vagrant bash << EOF
+mkdir -p ${HOME}/.kube
+sudo cp -i /etc/kubernetes/admin.conf ${HOME}/.kube/
+sudo chown "$(id -u)":"$(id -g)" ${HOME}/.kube/config
+EOF
+
+# Clean older config in shared folder
 if [[ -d "${ROOT_SHARED_FOLDER}" ]];
 then
 	rm -rf "${ROOT_SHARED_FOLDER}"
-	mkdir "${ROOT_SHARED_FOLDER}"
-else
-	# Ensure shared folder config exists
-	mkdir "${ROOT_SHARED_FOLDER}"
 fi
+# Ensure shared folder config exists
+mkdir "${ROOT_SHARED_FOLDER}"
 
 # Store k8s connection configuration file.
-cp "$HOME"/.kube/config "${ROOT_SHARED_FOLDER}/config"
+cp "${HOME}/.kube/config" "${ROOT_SHARED_FOLDER}/config"
 
-# Get join command and store in shared folder
+# Create script with kubernetes join command for workers.
 kubeadm token create --print-join-command > "${ROOT_SHARED_FOLDER}/worker-join.sh"
 
-# Install network plugin (Calico)
+###################
+# Network plugins #
+###################
+# Install Calico network plugin
 
 # Create k8s calico operator
 kubectl create -f https://projectcalico.docs.tigera.io/manifests/tigera-operator.yaml
@@ -55,9 +66,15 @@ sed -i 's#192.168.0.0/16#'"${POD_CIDR}"'#g' calico-custom-resources.yaml
 # Deploy Calico
 kubectl apply -f calico-custom-resources.yaml
 
+##################
+# Metrics server #
+##################
 # Install metrics server (Required for k8s dashboard)
 kubectl apply -f https://raw.githubusercontent.com/scriptcamp/kubeadm-scripts/main/manifests/metrics-server.yaml
 
+#############
+# Dashboard #
+#############
 # Deploy K8s dashboard
 kubectl apply -f "https://raw.githubusercontent.com/kubernetes/dashboard/${K8S_DASHBOARD_VERSION}/aio/deploy/recommended.yaml"
 
@@ -86,11 +103,5 @@ subjects:
 EOF
 
 # Get dashboard user tolen
-kubectl -n kubernetes-dashboard get secret $(kubectl -n kubernetes-dashboard get sa/admin-user -o jsonpath="{.secrets[0].name}") -o go-template="{{.data.token | base64decode}}" >  "${ROOT_SHARED_FOLDER}/dashboard-token"
+# kubectl -n kubernetes-dashboard get secret $(kubectl -n kubernetes-dashboard get sa/admin-user -o jsonpath="{.secrets[0].name}") -o go-template="{{.data.token | base64decode}}" >  "${ROOT_SHARED_FOLDER}/dashboard-token"
 
-sudo -i -u vagrant bash << EOF
-whoami
-mkdir -p /home/vagrant/.kube
-sudo cp -i /vagrant/configs/config /home/vagrant/.kube/
-sudo chown 1000:1000 /home/vagrant/.kube/config
-EOF
